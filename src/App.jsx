f4e595e636f41a6b77d7475713b330fc9d4c04cc
Brian Aryansyah <brianaryansyah@users.noreply.github.com>
2026-08-13 21:09:45 +0700

    Update root route to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
 import WeatherApp from './WeatherApp'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<WeatherApp />} />
         <Route path="/app" element={<WeatherApp />} />
       </Routes>
     </BrowserRouter>
