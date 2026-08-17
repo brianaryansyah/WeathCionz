225047319d2de29ee7ac1ed710ae4f22800ec18b
Brian Aryansyah <brianaryansyah@users.noreply.github.com>
2026-08-17 22:06:24 +0700

    Remove password authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import WeatherApp from './WeatherApp';
-import Login from './pages/Login';
-import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './contexts/AuthContext';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route 
-            path="/app" 
-            element={
-              <ProtectedRoute>
-                <WeatherApp />
-              </ProtectedRoute>
-            } 
-          />
-          <Route path="/" element={<Navigate to="/app" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WeatherApp />} />
+        <Route path="/app" element={<WeatherApp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
